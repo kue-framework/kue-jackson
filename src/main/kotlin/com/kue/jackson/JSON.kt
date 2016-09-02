e471f6a560029cd7a8d3dd902a150706c939891b
Michael Vaughan <mike.vaughan@qonceptual.com>
2016-09-01 21:08:02 -0400

    Added JSR 310 Jackson module by default.

--- src/main/kotlin/com/kue/jackson/JSON.kt
+++ src/main/kotlin/com/kue/jackson/JSON.kt
@@ -1,10 +1,15 @@
 package com.kue.jackson
 
 import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
 
 /**
  * @author Michael Vaughan
  */
 object JSON : ObjectMapper() {
+
+    init {
+        this.registerModule(JavaTimeModule())
+    }
 
 }
